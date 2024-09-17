ab2c85e9260e4c6f775956fedd8b87037c9a90e5
caglarorhan <caglaror@gmail.com>
2024-09-17 03:21:15 -0500

    refactor: all methods

--- js/content.js
+++ js/content.js
@@ -2,11 +2,10 @@ const browser = window.browser || window.chrome;
 const tweakter = {
     scrollAmount:300, // Threshold to trigger ad removal (in pixels)
     lastScrollY:0, // Store the previous scroll position
+    isTargetObserverActive: false,
     tweaks:{
         ads: false,
-        foryou: false,
-        trending: true,
-        notifications:true
+        foryou: false
     },
     async init(){
         this.utils.activateListeners();
@@ -31,10 +30,12 @@ const tweakter = {
                 tweakter.tweakRunners[key]();
             }
         });
+        // Set Observer for ads
+        this.utils.activateObservers();
     },
     tweakRunners:{
             ads(){
-                window.addEventListener("load", removeAds); // Call once on page load
+                window.addEventListener("load", tweakter.jobs.removeAds); // Call once on page load
                 window.addEventListener("scroll", onScroll); // Call onScroll function on scroll
                 function onScroll() {
                     const currentScrollY = window.scrollY; // Get current scroll position
@@ -42,16 +43,8 @@ const tweakter = {
                     // Check if scrolled amount is greater than or equal to the threshold
                     // and the scroll direction has changed (prevents excessive calls)
                     if (Math.abs(currentScrollY - tweakter.lastScrollY) >= tweakter.scrollAmount) {
-                        removeAds();
+                        tweakter.jobs.removeAds();
                         tweakter.lastScrollY = currentScrollY; // Update last scroll position
-                    }
-                }
-                function removeAds(){
-                    console.log('Twitter ad remover function called!');
-                    if (document.querySelector('[data-testid=placementTracking]')) {
-                        document.querySelectorAll('[data-testid=placementTracking] article').forEach(ad => {
-                            ad.parentNode.parentNode.remove();
-                        });
                     }
                 }
             },
@@ -66,22 +59,39 @@ const tweakter = {
     },
     jobs:{
         updateTweaks(payload){
-            console.log("Dogru tabi yakaladi");
+            console.log("Dogru tabi yakaladi", payload);
             Object.entries(payload).forEach(([key, value])=>{
                 tweakter.tweaks[key] = value;
             });
             tweakter.saveTweaks();
             tweakter.runTweaks();
+        },
+        getTweaks(){
+            console.log("getTweaks talebi geldi ve yanit gonderiliyor");
+            tweakter.utils.sendMessageToPopup({type:"updateCheckboxes", payload:JSON.stringify(tweakter.tweaks)});
+            return true;
+        },
+        removeAds(){
+            //console.log('Twitter ad remover function called!');
+            if (document.querySelector('[data-testid=placementTracking]')) {
+                document.querySelectorAll('[data-testid=placementTracking] article').forEach(ad => {
+                    ad.parentNode.parentNode.remove();
+                });
+            }
         }
     },
     utils:{
         sendMessageToPopup(messageObject){browser.runtime.sendMessage(messageObject)},
         activateListeners(){
-            browser.runtime.onMessage.addListener(async (request)=>{
-                console.log(request);
+            browser.runtime.onMessage.addListener( (request)=>{
+                //console.log(request);
                 let jobType = request.type;
+                //console.log(jobType);
                 let payload = JSON.parse(request.payload);
+                //console.log(payload);
+                tweakter.jobs[jobType](payload);
                 if(tweakter.jobs[jobType]){
+                    //console.log(jobType, " job found and running");
                     tweakter.jobs[jobType](payload)
                 }
                 else{
@@ -90,6 +100,34 @@ const tweakter = {
                 
             })
         },
+        activateObservers(){
+            if (this.isTargetObserverActive) {
+                console.log("Observer is already active.");
+                return;
+            }
+    
+            const checkForTargetNode = () => {
+                const targetNode = document.querySelector('[role="region"]');
+                if (targetNode) {
+                    const config = { childList: true, subtree: true };
+                    const callback = function(mutationsList, observer) {
+                        for (let mutation of mutationsList) {
+                            if (mutation.type === 'childList') {
+                                tweakter.jobs.removeAds();
+                            }
+                        }
+                    };
+                    const observer = new MutationObserver(callback);
+                    observer.observe(targetNode, config);
+                    this.isTargetObserverActive = true;
+                    console.log("Observer has been set up.");
+                } else {
+                    console.log("Target node not found, retrying...");
+                    setTimeout(checkForTargetNode, 500); // Retry after 1 second
+                }
+            };
+            checkForTargetNode();
+        }
 
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 console.log("This will be the index.js file");
 const browser = window.browser || window.chrome;
 let tweakter ={
-    targetDomains: ["twitter.com","x.com"],
+    targetDomain: "x.com",
     init(){
         console.log("Tweakter initialized");
         this.commonJobs();
@@ -10,9 +10,10 @@ let tweakter ={
 
     browser.tabs.query({}, function (tabs) {
         console.log(tabs)
-        let targetTab = tabs.find(tab => tab.url.includes("x.com"));
-        //console.log("Evet hedef tabimiz bulundu")
+        let targetTab = tabs.find(tab => tab.url.includes(tweakter.targetDomain));
+        console.log(targetTab);
         if (targetTab) {
+            console.log("Evet hedef tabimiz bulundu")
             browser.tabs.sendMessage(targetTab.id, messageObject);
         } else {
             console.log("No tab found with the specified URL.");
@@ -22,24 +23,30 @@ let tweakter ={
     },
     commonJobs(){
         browser.runtime.onMessage.addListener(async (request)=>{
-            this.jobs[request.type](request.payload)
+            let jobType = request.type;
+            let payload = request.payload;
+            this.jobs[jobType](payload)
         })
 
         document.querySelectorAll('input[type="checkbox"]').forEach((checkbox)=>{
                 checkbox.addEventListener('change', (e)=>{
                     let checks = {};
                     document.querySelectorAll('input[type="checkbox"]').forEach((checkbox)=>{
-                        checks[checkbox.name] = checkbox.checked;
+                        console.log("Deger degisti:", checkbox.id, checkbox.checked);
+                        checks[checkbox.id] = checkbox.checked;
                     })
                     this.sendMessageToContent({type:"updateTweaks", payload:JSON.stringify(checks)});
                 })
         })
+
+        this.sendMessageToContent({type:"getTweaks", payload:true});
     },
     jobs:{
         updateCheckboxes(payload){
+            console.log(payload);
             let checks = JSON.parse(payload);
             Object.entries(checks).forEach(([key, value])=>{
-                document.getElementByName(key).checked = value;
+                document.getElementById(key).checked = value?"checked":"";
             })
         },
     }
